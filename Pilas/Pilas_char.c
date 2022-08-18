97d8a1683017a3689d79d6048698337bbdfb6871
francisco-9 <francisco2014@gmail.com>
2022-08-18 18:04:52 -0300

    ejercicios de pilas agregados

---
+++ Pilas/Pilas_char.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estructuras_char.h"
+
+int main(void) {
+
+puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+Pila * p = (Pila *) malloc(sizeof(Pila));
+inicializacion(p);
+
+//Ingreso elementos
+char cadena[10] = "francisco";
+
+for (int i = 0; i < 9; i++){
+push(p, cadena[i]);
+}
+
+mostrar(p);
+
+for (int x = 0; x < 9; x++){
+cadena[x] = pop(p);
+}
+printf("%s", cadena);
+
+return 0;
+}
---
+++ Pilas/Pilas_func_suma.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estructuras.h"
+
+void suma(Pila *p){
+/*
+int valor1 = pop(p);
+int valor2 = pop(p);
+int r = valor1 + valor2;
+*/
+push(p, pop(p) + pop(p));
+}
+
+int main(void) {
+
+Pila * p = (Pila *) malloc(sizeof(Pila));
+inicializacion(p);
+
+push(p,4);
+push(p,8);
+suma(p);
+int r = pop(p);
+printf("La suma es: %d", r);
+
+
+return 0;
+}
