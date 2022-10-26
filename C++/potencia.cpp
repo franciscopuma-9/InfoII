bf8c877fc1d25d9dff4c7db1aaa15b4fce78fe74
francisco-9 <francisco2014@gmail.com>
2022-10-25 22:57:10 -0300

    26/10

--- C++/potencia.cpp
+++ C++/potencia.cpp
@@ -2,16 +2,38 @@
 
 using namespace std;
 
-int Potencia(int a){return a*a;}
+int Potencia(int a){return a*a;} //por valor
+//res = Potencia(a)
 
-void Potencia(int *p){*p = *p * *p;}
+void Potencia(int *p){*p = *p * *p;} //por referencia de puntero //pone la potencia en la misma posicion de memoria
+//res = Potencia(&p)
 
+int Potencia_ref(int &r){r = r * r;} //por referencia 
+//res = Potencia(a)  //puede tener problemas en sobrecarga de funciones porque se pasa igual que por valor
 
 int main (){
-    int p;
-    p = 4;
+    
+    int a = 4;
+    int *p = &a;
+
     Potencia(p);
-    cout<< *p;
+    cout << *p<< endl;
+
+    int b = 5;
+
+    int res = Potencia(b); //valor
+    cout << res << endl;
+
+    Potencia(&b); // puntedo
+    cout << b << endl;
+
+    Potencia_ref(b);//por referencia
+    cout << b << endl;
+
+    
+
+
+
 
     return 0;
 }
